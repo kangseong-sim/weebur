00f71749c96099326c67d8e693859fb5e6262da1
kangseongsim <stylesha34@gmail.com>
2025-04-21 02:41:32 +0900

    feat: React Query 클라이언트 제공을 위한 Provider 컴포넌트 추가

---
+++ components/Provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // default: true
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
