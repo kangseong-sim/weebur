3633c23a34ae8d1a393fe07399b64a401f720605
kangseongsim <stylesha34@gmail.com>
2025-04-21 03:50:33 +0900

    feat: 상품 상세 조회 기능 추가 및 타입 수정

--- service/ProductService.ts
+++ service/ProductService.ts
@@ -1,23 +1,22 @@
-import { Product } from "@/app/products/_components/ProductList";
 import http from "@/lib/http";
+import { Product, ProductsResponse } from "@/lib/type";
 import { ParsedQueryParams } from "@/utils/QueryString";
-
-type ProductResponse = {
-  products: Product[];
-  total: number;
-  skip: number;
-  limit: number;
-};
 
 //  상품 목록 조회
 const getProducts = (params: ParsedQueryParams) => {
   const hasQuery = !!params.q;
   const endpoint = hasQuery ? "/products/search" : "/products";
-  return http.get<ProductResponse>(endpoint, { params });
+  return http.get<ProductsResponse>(endpoint, { params });
+};
+
+//  상품 상세 조회
+const getProductDetail = (id: string) => {
+  return http.get<Product>(`products/${id}`);
 };
 
 const ProductService = {
   getProducts,
+  getProductDetail,
 };
 
 export default ProductService;
