b806fdcfb1372a28f8f365250aae43307956d748
kangseongsim <stylesha34@gmail.com>
2025-04-21 04:11:22 +0900

    fix: axios 기본 URL 수정

--- lib/http.ts
+++ lib/http.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const http = axios.create({
-  // baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   baseURL: "https://dummyjson.com",
   headers: {
     "Content-type": "application/json",
